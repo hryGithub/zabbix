3b9eea8e8d6ff9ac60f84eb4bd4a09ad4ef40541
hujian <hujian@tigerft.com>
2019-04-10 15:15:31 +0800

    修改微信告警脚本

--- scripts/weixin.py
+++ scripts/weixin.py
@@ -1,48 +1,40 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-#comment: zabbix接入微信报警脚本
+# -*- coding: utf-8 -*-
+# comment: zabbix微信报警
 
 import requests
 import sys
-import os
 import json
-import logging
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s, %(filename)s, %(levelname)s, %(message)s',
-    datefmt='%a, %d %b %Y %H:%M:%S',
-    filename=os.path.join('/tmp', 'weixin.log'),
-    filemode='a')
-
-corpid = 'ww7dd0074bd8b006f9'
-appsecret = '8gPCvguwomL0WMej8fcghxQgOY-y0LlGQsAicaDHvA8'
+corpid = 'ww3aa1f0b45e17b71d'
+appsecret = 'JHu8DYErPvHDC-Vxae47aQOCHVpSi0ct_V2XfNYAtCE'
 agentid = 1000002
-#获取accesstoken
-token_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' + corpid + '&corpsecret=' + appsecret
-req = requests.get(token_url)
-accesstoken = req.json()['access_token']
 
-#发送消息
-msgsend_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + accesstoken
 
-touser = sys.argv[1]
-subject = sys.argv[2]
-#toparty='3|4|5|6'
-message = sys.argv[2] + "\n\n" + sys.argv[3]
+def alert(touser, subject, message, toparty=1):
+    # 获取accesstoken
+    token_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' + corpid + '&corpsecret=' + appsecret
+    req = requests.get(token_url)
+    accesstoken = req.json()['access_token']
 
-params = {
-    "touser": touser,
-    #       "toparty": toparty,
-    "msgtype": "text",
-    "agentid": agentid,
-    "text": {
-        "content": message
-    },
-    "safe": 0
-}
+    # 发送消息
+    msgsend_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + accesstoken
 
-req = requests.post(msgsend_url, data=json.dumps(params))
+    message = subject + "\n\n" + message
+        
+    params = {
+        "touser": touser,
+        "toparty": toparty,
+        "msgtype": "text",
+        "agentid": agentid,
+        "text": {
+            "content": message
+        },
+        "safe": 0
+    }
 
-logging.info('sendto:' + touser + ';;subject:' + subject + ';;message:' +
-             message)
+    req = requests.post(msgsend_url, data=json.dumps(params))
+
+
+if __name__ == "__main__":
+    alert(sys.argv[1], sys.argv[2], sys.argv[3])
